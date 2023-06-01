949b7ec7862fc8248d99af343c2230298e0f558e
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2023-06-02 04:44:24 +0500

    theme

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#FFF42C",
     },
     secondary: {
       main: "#19857b",
